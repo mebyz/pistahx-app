ec1732b6612c8c384fafda77979c141730d2f827
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-05-27 23:22:20 +0200

    clean root

--- deploydocker.sh
+++
@@ -1,8 +0,0 @@
-cd ../pistahx-app_builds
-git reset --hard HEAD
-cp ../pistahx/distrib/out/* app/
-cp ../pistahx/distrib/package.json app/
-cp ../pistahx/Dockerfile .
-git add *
-git commit
-git push
